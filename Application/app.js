9e61a78190472e8c2a3c068866f9fc8fb9c7e316
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-04-06 15:21:17 -0400

    Created seed file for the database; cleaned up file structure

--- Application/app.js
+++ Application/app.js
@@ -1,3 +1,18 @@
+// Dependencies, must be installed first
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require("console.table");
+
+// Create connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Chidi17",
+  database: "employees_db",
+});
+
+// Initialize connection
+connection.connect((err) => {
+  if (err) throw err;
+});
